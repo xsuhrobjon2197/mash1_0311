df73348d8813df433dcd4baa2b7156e91375323b
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-03 05:08:42 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+#1-misol
+sum = 0
+for i in range(1,11):
+    sum += i
+
+print(sum)
+
+#2-misol
+sum = 0
+for i in range(1,51):
+   if i % 2 != 0:
+    sum += i
+
+print(sum)
+
+#3-misol
+sum = 0
+for i in range(2,51):
+    if i % 2 == 0:
+     sum += i
+
+print(sum)
+
+#4-misol
+sum = 0
+for i in range(10,31):
+    sum += i
+
+print(sum)
+
+#5-misol
+n = int(input("n ni kirit: "))
+sum = 0
+for i in range(1,n+1):
+    sum += i
+
+print(sum)
+
+# 6-misol
+sum = 0
+for i in range(1,21):
+    sum += i * i
+
+print(sum)
+
+#7-misol
+sum = 0
+for i in range(5,26):
+    sum += i
+
+print(sum)
+
+#8-misol
+sum = 0
+for i in range(1,16):
+    if i % 3 == 0:
+        sum += i
+
+print(sum)
+
+# 9-misol
+sum = 0
+for i in range(50,101):
+    sum += i
+
+print(sum)
+
+#10-misol
+sum = 0
+for i in range(1,31):
+    if i % 5 == 0:
+     sum += i
+
+print(sum)
